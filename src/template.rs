781704288c3fd4ae4e1ae0691c78d00c2ff5f1c6
Dorian Thiessen (dbt908) <dorian.thiessen@usask.ca>
2019-01-04 10:09:52 -0600

    Working

--- src/template.rs
+++ src/template.rs
@@ -1,18 +1,23 @@
 #[repr(C)]
 #[derive(Debug)]
 pub struct Apple {
-    pub f_num: f32,
-    pub i_num: i32,
-    //pub u_num: u32,
+    pub f: f32,
+    pub i: i32,
+    //pub u_num: u32, // Broken?
 }
 
 // TODO: To what degree, if any, can Traits be used across the ffi?
 // TODO: PySync Trait?
 
+/*#[no_mangle] // returns the size of the struct in bytes
+pub extern "C" fn sizeof_apple() -> i32 {
+    size_of::<apple>();
+}*/
+
 #[no_mangle]
 pub extern "C" fn new_apple() -> *mut Apple {
-    let r = Box::new(Apple { f_num: 2.89, i_num: -13});
-    println!("Rust: Allocating {} bytes for Apple instance", std::mem::size_of::<Apple>());
+    let r = Box::new(Apple { f: 2.89, i: -13});
+    println!("Rust: Allocating {} bytes for apple instance", std::mem::size_of::<Apple>());
     let raw = Box::into_raw(r);
     raw
 }
@@ -27,57 +32,44 @@ pub extern "C" fn free_apple(ptr: *mut Apple) {
     }
 }
 
-/*fn fix_ptr<'a>(ptr: *const Apple) -> &'a Apple {
-    unsafe {
-        assert!(!ptr.is_null());
-        &*ptr
-    }
-}*/
+// Getters and Setters
 
 #[no_mangle]
 pub extern "C" fn get_f(ptr: &Apple) -> f32 {
-    //println!("Rust::get_float::<Apple> = {:?}", ptr);
-    //println!("Rust::get_float() = {}", ptr.f_num);
-    ptr.f_num
+    ptr.f
 }
 
-/*#[no_mangle]
-pub extern "C" fn get_int(ptr: &mut Apple) -> i32 {
+#[no_mangle]
+pub extern "C" fn set_f(ptr: &mut Apple, value: f32) {
+    ptr.f = value
+}
+
+#[no_mangle]
+pub extern "C" fn get_i(ptr: &Apple) -> i32 {
     ptr.i
 }
 
 #[no_mangle]
-pub extern "C" fn get_uint(ptr: &mut Apple) -> u32 {
-    ptr.u
-}*/
-
-#[no_mangle]
-pub extern "C" fn set_f(ptr: &mut Apple, value: f32) {
-    ptr.f_num = value
+pub extern "C" fn set_i(ptr: &mut Apple, value: i32) {
+    ptr.i = value
 }
 
-//println!("POST: Rust::get_float::<Apple> = {:?}", ptr);
-//println!("PRE: Rust::get_float::<Apple> = {:?}", ptr);
+// Other
 
-/*#[no_mangle]
-pub extern "C" fn set_int(ptr: &mut Apple, value: i32) {
-    ptr.i = value
+#[no_mangle]
+pub extern "C" fn display(ptr: &Apple) {
+    println!("Rust: {:?}", ptr)
+}
+
+// Broken?
+
+/*
+#[no_mangle]
+pub extern "C" fn get_uint(ptr: &mut Apple) -> u32 {
+    ptr.u
 }
 
 #[no_mangle]
 pub extern "C" fn set_uint(ptr: &mut Apple, value: u32) {
     ptr.u = value
 }*/
-
-// Other
-
-/*
-#[no_mangle]
-pub extern "C" fn display(ptr: *const Apple) {
-    let v = unsafe {
-        assert!(!ptr.is_null());
-        &*ptr
-    };
-    println!("Rust: {:?}", v)
-}
-*/
--- template.py
+++ template.py
@@ -3,15 +3,20 @@ from cffi import FFI
 # The C-equivalent of the Rust struct and function types.
 header_apple = """
     typedef struct {
-        float       f_num;
-        int         i_num;
+        float       f;
+        int         i;
     } apple;
 
     apple new_apple();
     void free_apple(apple);
 
+    void display(apple);
+
     void set_f(apple, float);
     float get_f(apple);
+
+    void set_i(apple, int);
+    int get_i(apple);
 """
 
 class RustLib():
@@ -19,17 +24,50 @@ class RustLib():
         self.ffi = FFI()
         self.lib = self.ffi.dlopen(lib_in)
         self.ffi.cdef(headers_in)
+
+    # Access function from lib, ex: _.fn().somefunction()
     def fn(self):
         return self.lib
 
     # Rust structures exposed by the library
-    #def build_apple(self):
-    #    ptr = self.ffi.new("apple *")#self.lib.new_apple()
-    #    print("ptr = " + str(ptr))
-    #    sa = apple(self.lib, ptr)
-    #     return sa
+
+    # For when initialization code is defined in Rust
+    def build_apple(self):
+        ptr = self.lib.new_apple()
+        return Apple(lib_in = self.lib, ptr_in = ptr)
+
+    # For when initialization code is defined in Python
+    def new_apple(self):
+        ptr = self.ffi.new("apple *")
+        assert(ptr != self.ffi.NULL)
+        return Apple(lib_in = self.lib, ptr_in = ptr)
 
     # additional structs here...
+
+'''Example Rust Struct Wrapper'''
+class Apple():
+    def __init__(self, lib_in, ptr_in):
+        self.ptr = ptr_in  # Store pointer being wrapped and
+        self._lib = lib_in # library for access to functions
+
+    def display(self):
+        self._lib.display(self.ptr)
+
+    def free(self):
+        self._lib.free(self.ptr)
+
+    # Convenience methods for accessing rust functions
+    def get_float(self):
+        return self._lib.get_f(self.ptr)
+
+    def set_float(self, v):
+        self._lib.set_f(self.ptr, v)
+
+    def get_int(self):
+        return self._lib.get_i(self.ptr)
+
+    def set_int(self, v):
+        self._lib.set_i(self.ptr, v)
 
 #   ============
 #   Load Library
@@ -40,36 +78,65 @@ class RustLib():
 #   Linux: *.so
 win_lib = "target/debug/rusty_python.dll"
 #headers = header_apple # add additional headers here...
-rust = RustLib(win_lib, header_apple)
+rust = RustLib(lib_in = win_lib, headers_in = header_apple)
 
 #   =================
 #   Application Logic
 #   =================
 
-sa = rust.fn().new_apple()
-print("new_apple() = " + str(sa))
+# To identify which language a print
+# statement was written at runtime
+def printPy(msg):
+    print("Python: " + msg)
 
-# Check initial value
-f = rust.fn().get_f(sa)
-print("sa.float = (" + str(f) +") <- initial value")
-# Change, confirm it changed
-rust.fn().set_f(sa, 8)
-f = rust.fn().get_f(sa)
-print("sa.float = (" + str(f) + ") <- should be 5.1")
+def unwrapped():
+    sa = rust.fn().new_apple()
+    printPy("ptr = " + str(sa))
 
+    # Check initial value
+    f = rust.fn().get_f(sa)
+    printPy("ptr.f = (" + str(f) +") <- initial value")
+    # Change, confirm it changed
+    rust.fn().set_f(sa, 0.25)
+    f = rust.fn().get_f(sa)
+    assert(f == 0.25)
+    printPy("ptr.f = (" + str(f) + ") <- should be 0.25")
 
-#sa = rust_lib.build_apple();
-#print("sa = " + str(sa.ptr))
-#sa.display()
+    # Check initial value
+    i = rust.fn().get_i(sa)
+    printPy("ptr.i = (" + str(i) +") <- initial value")
+    # Change, confirm it changed
+    rust.fn().set_i(sa, 9)
+    i = rust.fn().get_i(sa)
+    assert(i == 9)
+    printPy("ptr.i = (" + str(i) + ") <- should be 9")
 
-#sa.set_float(6.66)
-#f = sa.get_float()
-#print("sa.float = " + str(f))
+    rust.fn().display(sa)
 
-#sa.set_float(6.66)
-#i = sa.get_int()
-#print("sa.int = " + str(i))
+def wrapped():
+    sa = rust.build_apple();
+    #sa = rust.new_apple();
+    printPy("sa = " + str(sa))
+    printPy("sa.ptr = " + str(sa.ptr))
 
-#sa.set_float(6.66)
-#u = sa.get_uint()
-#print("sa.uint = " + str(u))
+    f = sa.get_float()
+    printPy("sa.f = " + str(f) + " <- initial value")
+    sa.set_float(0.25)
+    f = sa.get_float()
+    printPy("sa.f = " + str(f) + " <- should be 0.25")
+    assert(f == 0.25)
+
+    i = sa.get_int()
+    printPy("sa.i = " + str(i) + " <- initial value")
+    sa.set_int(9)
+    i = sa.get_int()
+    printPy("sa.i = " + str(i) + " <- should be 9")
+    assert(i == 9)
+
+    sa.display()
+
+print("\nUN-WRAPPED:")
+unwrapped()
+
+print("\nWRAPPED:")
+wrapped()
